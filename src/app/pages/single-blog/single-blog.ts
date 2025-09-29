357430a9ee22cd82270b8e1317a4baaec3d7b157
samaki8 <samira08@gmail.com>
2025-09-29 22:18:33 +0200

    Configuration django and add models: Visitor, User and Article.

---
+++ src/app/pages/single-blog/single-blog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-blog',
+  imports: [],
+  templateUrl: './single-blog.html',
+  styleUrl: './single-blog.scss'
+})
+export class SingleBlog {
+
+}
